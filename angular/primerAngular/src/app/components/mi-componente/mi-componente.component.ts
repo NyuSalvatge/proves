7a1d0be1cf7df23a2aa9ce05b23a582ee3ae9735
Nyu <music.nyu99999@gmail.com>
2019-12-16 15:54:40 +0100

    commit neiw

---
+++ angular/primerAngular/src/app/components/mi-componente/mi-componente.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+
+    selector: 'mi-componente',
+    template: `<h1>Hola mundo, soy MI COMPONENTE</h1>
+                <p>Este es mi primer componente</p>
+    `
+
+})
+
+export class MiComponente{
+    constructor(){
+        console.log("Componente cargado");
+    }
+}
