14388f42ae1333e3a763f0777e92dcc0a8150c3d
Nyu <albert.viader@gmail.com>
2019-12-16 18:12:51 +0100

    commit de primer component

--- angular/primerAngular/src/app/components/mi-componente/mi-componente.component.ts
+++ angular/primerAngular/src/app/components/mi-componente/mi-componente.component.ts
@@ -4,9 +4,8 @@ import { Component } from '@angular/core';
 @Component({
 
     selector: 'mi-componente',
-    template: `<h1>Hola mundo, soy MI COMPONENTE</h1>
-                <p>Este es mi primer componente</p>
-    `
+    templateUrl: '/mi-componente.component.html',
+    styleUrls: ['/mi-componente.component.css']
 
 })
 
